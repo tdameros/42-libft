1c0982c02f892bdc12a795468abd71a3b2770fa6
Tom Damerose <tdameros@z2r7p4.42lyon.fr>
2022-11-08 00:26:05 +0100

    secure putendl_fd

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: tdameros <tdameros@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 13:57:43 by tdameros          #+#    #+#             */
-/*   Updated: 2022/10/11 13:58:27 by tdameros         ###   ########lyon.fr   */
+/*   Updated: 2022/11/08 00:25:43 by tdameros         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_putendl_fd(char *s, int fd)
 {
+	if (s == NULL)
+		return ;
 	ft_putstr_fd(s, fd);
 	ft_putchar_fd('\n', fd);
 }
