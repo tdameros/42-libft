916c9435390dcc3387fc85bfd70366bd8699eee9
Tom-Drs <tomdamerose@gmail.com>
2022-11-08 23:25:17 +0100

    change libft include

--- ft_atoi.c
+++ ft_atoi.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 static int	count_white_spaces(const char *s);
 
