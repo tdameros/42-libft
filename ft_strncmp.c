de53729962e566a70080d1547982f14c14011fe3
Tom Damerose <tdameros@z4r8p1.42lyon.fr>
2022-11-09 11:35:27 +0100

    add stop in NULL terminate

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tdameros <tdameros@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 23:42:57 by tdameros          #+#    #+#             */
-/*   Updated: 2022/11/06 16:59:05 by tdameros         ###   ########lyon.fr   */
+/*   Updated: 2022/11/09 11:34:49 by tdameros         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,22 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	index;
+	size_t			index;
+	unsigned char	*s1_ptr;
+	unsigned char	*s2_ptr;
 
+	s1_ptr = (unsigned char *) s1;
+	s2_ptr = (unsigned char *) s2;
 	if (n == 0)
 		return (0);
 	index = 0;
-	while ((unsigned char) *s1 == (unsigned char) *s2 && index < n)
+	while (*s1_ptr == *s2_ptr && *s1_ptr != '\0' && *s2_ptr != '\0' && index < n)
 	{
-		s1++;
-		s2++;
+		s1_ptr++;
+		s2_ptr++;
 		index++;
 	}
 	if (index == n)
 		return (0);
-	return ((unsigned char) *s1 - (unsigned char) *s2);
+	return (*s1_ptr - *s2_ptr);
 }
