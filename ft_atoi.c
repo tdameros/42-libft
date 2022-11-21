edd47ca00648b5f317199f4c33d7685068ffc4fc
Tom-Drs <tomdamerose@gmail.com>
2022-11-22 00:33:36 +0100

    work in progress ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,15 +6,15 @@
 /*   By: tdameros <tdameros@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 15:44:18 by tdameros          #+#    #+#             */
-/*   Updated: 2022/11/14 23:48:45 by tdameros         ###   ########lyon.fr   */
+/*   Updated: 2022/11/22 00:33:00 by tdameros         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <limits.h>
 
-static int	count_white_spaces(const char *s);
-static int	check_overflow(const char *str, int result, int sign);
+static size_t	count_white_spaces(const char *s);
+static int		check_overflow(const char *str, size_t index, int result, int sign);
 
 int	ft_atoi(const char *str)
 {
@@ -35,17 +35,19 @@ int	ft_atoi(const char *str)
 		str++;
 	while (ft_isdigit(str[index]))
 	{
-		if (index == 9)
-			return (check_overflow(str, result, sign));
+		if (LONG_MAX / 10 < result * sign || LONG_MAX - (str[index] - '0') < result * 10 * sign)
+			return ((int) LONG_MAX);
+		if (LONG_MIN / 10 > result * sign || LONG_MIN + (str[index] - '0') > result * 10 * sign)
+			return ((int) LONG_MIN);
 		result = result * 10 + (str[index] - '0');
 		index++;
 	}
 	return (result * sign);
 }
 
-static int	count_white_spaces(const char *s)
+static size_t	count_white_spaces(const char *s)
 {
-	int	index;
+	size_t	index;
 
 	index = 0;
 	while (s[index] == ' ' || s[index] == '\f' || s[index] == '\n'
@@ -54,15 +56,12 @@ static int	count_white_spaces(const char *s)
 	return (index);
 }
 
-static int	check_overflow(const char *str, int result, int sign)
+static int	check_overflow(const char *str, int index,int result, int sign)
 {	
-	if (ft_isdigit(str[10]))
-		return (-1);
-	if (result >= INT_MAX / 10 && sign == 1 && str[9] > '7')
-		return (-1);
-	if (result >= INT_MAX / 10 && sign == -1 && str[9] > '8')
-		return (-1);
-	if (result == INT_MAX / 10 && sign == -1 && str[9] == '8')
-		return (INT_MIN);
-	return ((result * 10 + (str[9] - '0')) * sign);
+	if (LONG_MAX / 10 < result * sign
+		|| LONG_MAX - (str[index] - '0') < result * 10 * sign)
+		return ((int) LONG_MAX);
+	if (LONG_MIN / 10 > result * sign
+		|| LONG_MIN + (str[index] - '0') > result * 10 * sign)
+		return ((int) LONG_MIN);
 }
